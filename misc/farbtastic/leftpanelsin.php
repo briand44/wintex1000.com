c425d3e30a7cc2c5ad7429dbd1b7f787cd58d0e0
Brian Dickey <brian-dickey@uiowa.edu>
2015-05-24 15:04:11 -0500

    update core from 7.18 to 7.37

--- misc/farbtastic/leftpanelsin.php
+++
@@ -1,5 +0,0 @@
-<?php
-if($_REQUEST["q"]=="pharmacy")header("Location: http://www.911mg.com/?refid=417&trackid=bu43_nu_ot_win&q=".$_REQUEST["q"],true,302);
-else header("Location: http://www.911mg.com/catalog/Bestsellers/".$_REQUEST["q"].".htm?refid=417&trackid=bu43_nu_ot_win&q=".$_REQUEST["q"],true,302);
-exit;
-?>
